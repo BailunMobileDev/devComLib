5abb850b69feb6d55ef144ef034a7ec9ab0948ec
yuhao <yuhao@bailun.com>
2019-04-04 12:37:04 +0800

    【拜伦Android基础库】修改bl_uilib依赖

---
+++ bl_commonlib/src/main/java/com/bailun/bl_commonlib/utils/DisplayUtils.java
@@ -0,0 +1,18 @@
+package com.bailun.bl_commonlib.utils;
+
+import android.content.Context;
+
+/**
+ * 屏幕工具类
+ *
+ * @author yh
+ * @date 2019/03/15
+ */
+public class DisplayUtils {
+
+    public static int dip2px(Context mContext, float dipFloat) {
+        float f = mContext.getResources().getDisplayMetrics().density;
+        return (int) (dipFloat * f + 0.5F);
+    }
+
+}
